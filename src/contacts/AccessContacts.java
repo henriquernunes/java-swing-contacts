933af4fe7e98a9b5a39724acc3aab7ffc6433e7f
Henrique Nunes <henriquerosanunes@hotmail.com>
2025-11-07 15:11:30 -0300

    feat: atulizado as variveis de firstName e lastName, criado o metodo para salvar os contatos no 'list.txt';

---
+++ src/contacts/AccessContacts.java
@@ -0,0 +1,40 @@
+package contacts;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccessContacts {
+  private String path = "list.txt";
+
+  public List<Contact> list() {
+    List<Contact> list = new ArrayList<>();
+
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+      String line = br.readLine();
+
+      while (line != null) {
+        String[] fields = line.split(";");
+        list.add(new Contact(fields[0], fields[1], fields[2]));
+        line = br.readLine();
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return list;
+  }
+
+  public void saveContacts(List<Contact> contacts) {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+      for (Contact c : contacts) {
+        bw.write(c.getFirstName() + ";" + c.getLastName() + ";" + c.getPhone());
+        bw.newLine();
+      }
+    } catch (Exception e) {
+      System.out.println("Erro ao salvar contatos: " + e.getMessage());
+    }
+  }
+}
---
+++ src/contacts/Contact.java
@@ -0,0 +1,46 @@
+package contacts;
+
+public class Contact {
+  private String firstName;
+  private String lastName;
+  private String phone;
+
+  public Contact(String firstName, String lastName, String phone) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getFullName() {
+    return firstName + " " + lastName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  @Override
+  public String toString() {
+    return firstName + " " + lastName + " - " + phone;
+  }
+}
